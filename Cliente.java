50051778d5ef414d988ff888107bb7292c363a3a
GustavoBraghin <62820751+GustavoBraghin@users.noreply.github.com>
2021-04-23 20:12:34 -0300

    Add files via upload
    Classe cliente e main atualizadas

--- Cliente.java
+++ Cliente.java
@@ -1,17 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package locadoradecarros;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Cliente {
-    
+
     private String nome;
     private String telefone;
     private String email;
     private String cpf;
     private String endereco;
+    static List<Cliente> clientes = new ArrayList<Cliente>();
 
     public Cliente(String nome, String telefone, String email, String cpf, String endereco) {
         this.nome = nome;
@@ -63,9 +64,68 @@ public class Cliente {
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
-    
-    
 
-    
-    
+    //MÉTODOS USADOS NO CRUD CLIENTE DA CLASSE CLIENTE
+    public void Cadastrar(Cliente cliente) {
+        Cliente.clientes.add(cliente);
+    }
+
+    public Cliente Consultar(String cpf) {
+        int n = clientes.size();
+
+        for (int i = 0; i < n; i++) {
+            Cliente c = clientes.get(i);
+
+            //COMPARA SE O CPF DO CLIENTE DA LISTA É IGUAL AO CPF DIGITADO PELO USUÁRIO  E RETORNA O CLIENTE SE TRUE
+            if (cpf.equals(c.getCpf())) {
+                System.out.print("Existente.");
+                return c;
+            }
+
+        }
+        return null;
+    }
+
+    public void Atualizar(Cliente cliente) {
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < Cliente.clientes.size(); i++) {
+            if (cliente.equals(Cliente.clientes.get(i))) {
+
+                System.out.println("Nome:");
+                nome = scan.next();
+                Cliente.clientes.get(i).setNome(nome);
+                System.out.println("Telefone:");
+                telefone = scan.next();
+                Cliente.clientes.get(i).setTelefone(telefone);
+                System.out.println("email:");
+                email = scan.next();
+                Cliente.clientes.get(i).setEmail(email);
+                System.out.println("CPF:");
+                cpf = scan.next();
+                Cliente.clientes.get(i).setCpf(cpf);
+                System.out.println("Endereço:");
+                endereco = scan.next();
+                Cliente.clientes.get(i).setEndereco(endereco);
+
+                Cliente.clientes.set(i, cliente);
+                System.out.println("Nome:" + Cliente.clientes.get(i).getNome());
+
+                System.out.println("Tel:" + Cliente.clientes.get(i).getTelefone());
+
+                System.out.println("Email:" + Cliente.clientes.get(i).getEmail());
+
+                System.out.println("Cpf:" + Cliente.clientes.get(i).getCpf());
+
+                System.out.println("end:" + Cliente.clientes.get(i).getEndereco());
+
+            }
+        }
+    }
+
+    public void Remover(Cliente cliente) {
+        Cliente.clientes.remove(cliente);
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,131 +1,205 @@
 package locadoradecarros;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    
+
     //CRUD PRINCIPAL
-    public static int Crud(){
-        
+    public static int Crud() {
+
         Scanner scan = new Scanner(System.in);
-        
+
         int opcao;
-        
-        do{
+
+        do {
             System.out.println("Locadora de Carros\n");
             System.out.println("1 - Cliente");
             System.out.println("2 - Carro");
             System.out.println("3 - Locação");
             System.out.println("0 - Sair");
             opcao = scan.nextInt();
-            
-            if(opcao < 0 || opcao > 3){
+
+            if (opcao < 0 || opcao > 3) {
                 System.out.println("Opção inválida\n");
             }
-        }while (opcao <0 || opcao > 3);
-        
+        } while (opcao < 0 || opcao > 3);
+
         return opcao;
     }
-    
+
     //CRUD DO CLIENTE
-    public static int CrudCliente(){
-        
+    public static int CrudCliente() {
+
         Scanner scan = new Scanner(System.in);
-        
+        Cliente cliente = new Cliente();
+        String nome;
+        String tel;
+        String email;
+        String cpf;
+        String end;
+        boolean existe;
         int opcao;
-        
-        do{
+
+        do {
             System.out.println("Cliente\n");
             System.out.println("1 - Cadastrar");
             System.out.println("2 - Consultar");
             System.out.println("3 - Atualizar");
             System.out.println("4 - Remover");
             opcao = scan.nextInt();
-            
-            if(opcao < 1 || opcao > 4){
+
+            if (opcao < 1 || opcao > 4) {
                 System.out.println("Opção inválida\n");
+            } else {
+                switch (opcao) {
+                    case 1://Cadastrar
+                        System.out.println("Nome:");
+                        nome = scan.next();
+
+                        System.out.println("Telefone:");
+                        tel = scan.next();
+
+                        System.out.println("email:");
+                        email = scan.next();
+
+                        System.out.println("CPF:");
+                        cpf = scan.next();
+
+                        System.out.println("Endereço:");
+                        end = scan.next();
+
+                        cliente.Cadastrar(new Cliente(nome, tel, email, cpf, end));
+                        //passando um cliente da lista para variavel c1
+                        //Cliente c1 = Cliente.clientes.get(0);
+                        break;
+                    case 2://Consultar
+                        System.out.println("Digite o CPF para consulta:\n");
+                        cpf = scan.next();
+                        clearBuffer(scan);
+                        Cliente clienteaux = cliente.Consultar(cpf);
+
+                        if (clienteaux == null) {
+                            System.out.println("Cpf não cadastrado.");
+                        } else {
+                            System.out.println(clienteaux.getCpf());
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("Digite o CPF para alteracao:\n");
+                        cpf = scan.next();
+                        clearBuffer(scan);
+                        Cliente clienteatt = cliente.Consultar(cpf);
+
+                        if (clienteatt == null) {
+                            System.out.println("Cpf não cadastrado.");
+                        } else {
+                            cliente.Atualizar(clienteatt);
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Digite o CPF para consulta:\n");
+                        cpf = scan.next();
+                        clearBuffer(scan);
+                        Cliente clienterem = cliente.Consultar(cpf);
+
+                        if (clienterem == null) {
+                            System.out.println("Cpf não cadastrado.\n");
+                        } else {
+                            cliente.Remover(clienterem);
+                            System.out.println("Cliente removido.\n");
+                        }
+                        break;
+                }
             }
-        }while (opcao < 1 || opcao > 4);
-        
+        } while (opcao < 1 || opcao > 4);
+
         return opcao;
     }
-    
+
     //CRUD DO CARRO
-    public static int CrudCarro(){
-        
+    public static int CrudCarro() {
+
         Scanner scan = new Scanner(System.in);
-        
+
         int opcao;
-        
-        do{
+
+        do {
             System.out.println("Carro\n");
             System.out.println("1 - Cadastrar");
             System.out.println("2 - Consultar");
             System.out.println("3 - Atualizar");
             System.out.println("4 - Remover");
             opcao = scan.nextInt();
-            
-            if(opcao < 1 || opcao > 4){
+
+            if (opcao < 1 || opcao > 4) {
                 System.out.println("Opção inválida\n");
             }
-        }while (opcao < 1 || opcao > 4);
-        
+        } while (opcao < 1 || opcao > 4);
+
         return opcao;
     }
-    
+
     //CRUD DA LOCAÇÃO
-    public static int CrudLocacao(){
-        
+    public static int CrudLocacao() {
+
         Scanner scan = new Scanner(System.in);
-        
+
         int opcao;
-        
-        do{
+
+        do {
             System.out.println("Locação\n");
             System.out.println("1 - Retirada");
             System.out.println("2 - Devolução");
             System.out.println("3 - Consulta");
             opcao = scan.nextInt();
-            
-            if(opcao < 1 || opcao > 3){
+
+            if (opcao < 1 || opcao > 3) {
                 System.out.println("Opção inválida\n");
             }
-        }while (opcao < 1 || opcao > 3);
-        
+        } while (opcao < 1 || opcao > 3);
+
         return opcao;
     }
-    
+
     //MAIN
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         int opCrud, opcao;
-        
+
         Carro c = new Carro();
-        
+
         //RODA O CRUD ATÉ QUE A OPÇÃO SAIR SEJA SELECIONADA
-        do{
+        do {
             opCrud = Crud();
-            
-            switch (opCrud){
+
+            switch (opCrud) {
                 //CASO ESCOLHA O MENU DO CLIENTE
-                case 1: 
+                case 1:
                     opcao = CrudCliente();
-                        //DESENVOLVER AQUI AS FUNCIONALIDADES DO CRUDCLIENTE()
+                    //DESENVOLVER AQUI AS FUNCIONALIDADES DO CRUDCLIENTE()
                     break;
                 //CASO ESCOLHA O MENU DO CARRO
                 case 2:
                     opcao = CrudCarro();
-                        //DESENVOLVER AQUI AS FUNCIONALIDADES DO CRUDCARRO()
+                    //DESENVOLVER AQUI AS FUNCIONALIDADES DO CRUDCARRO()
                     break;
-                    
+
                 case 3:
                     opcao = CrudLocacao();
-                        //DESENVOLVER AQUI AS FUNCIONALIDADES DO CRUDLOCACAO()
+                    //DESENVOLVER AQUI AS FUNCIONALIDADES DO CRUDLOCACAO()
                     break;
             }
-        }while(opCrud != 0);
-        
+        } while (opCrud != 0);
+
     }
-    
+
+    private static void clearBuffer(Scanner scan) {
+        if (scan.hasNextLine()) {
+            scan.nextLine();
+        }
+    }
 }
